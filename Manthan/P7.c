08f3ab164caf78476260f9a6cfc5dc9361027f27
Manthan <manthanvasoya07@gmail.com>
2025-08-19 11:09:14 +0530

    Update P7.c

--- Manthan/P7.c
+++ Manthan/P7.c
@@ -1,1 +1,30 @@
 #include <stdio.h>
+int main()
+{
+  int a,f;
+  printf("Enter Age: ");
+  scanf("%d",&a);
+  if(a<=12)
+    {
+      printf("Children\n");
+      printf("Ticket Fare: 0");
+      return 1;
+    }
+  else if(a<60)
+    {
+      printf("Adult\n");
+      printf("Ticket Fare: 300");
+      return 1;
+    }
+  else if(a<120)
+    {
+    printf("Senior Citizen\n");
+    printf("Ticket Fare: 0 ");
+    return 1;
+    }
+  else 
+    {
+      printf("You are DEAD");
+      return 0;
+    }
+}
