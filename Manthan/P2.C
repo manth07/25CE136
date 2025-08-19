8249e99353b944a0c93a1f91c0a447164c1c3a5c
Manthan <manthanvasoya07@gmail.com>
2025-08-19 10:49:56 +0530

    Add files via upload

---
+++ Manthan/P2.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int length,breadth,area,perimeter;
+   clrscr();
+
+   printf("Enter length: ");
+   scanf("%d",&length);
+
+   printf("Enter breadth: ");
+   scanf("%d",&breadth);
+
+   area=length*breadth;
+   printf("Your area is %d\n",area);
+
+   perimeter=2*(length+breadth);
+   printf("Your perimeter is %d",perimeter);
+   getch();
+
+}
+
---
+++ Manthan/P3.C
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+     float h,w,b;
+
+     clrscr();
+
+     printf("Enter Height:");
+     scanf("%f",&h);
+
+     printf("Enter Weight:");
+     scanf("%f",&w);
+
+     if (h<=0)
+     {
+      printf("Invalid Height\n");
+     }
+
+     if(w<=0)
+     {
+      printf("Invalid Weight\n");
+     }
+
+     b=w/(h*h);
+     printf("Your BMI is: %.2f \n",b);
+
+     if(b<18.5)
+     {
+      printf("You are Under weight");
+     }
+
+     else if (b<=24.99)
+     {
+      printf("You are Normal weight");
+     }
+
+     else if (b<=29.99)
+     {
+      printf("You are Over weight");
+     }
+     else if (b>=30)
+     {
+      printf("You are Obese");
+     }
+     getch();
+
+}
