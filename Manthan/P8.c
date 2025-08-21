bcfcbffede438ac9376d6c1d1792140fea31bf0e
Manthan <manthanvasoya07@gmail.com>
2025-08-21 14:49:06 +0530

    Add files via upload

---
+++ Manthan/P8.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter your age: ");
+    scanf("%d",&a);
+
+    if(a>0)
+    {
+        if(a>=18)
+        {
+            printf("You are eligible to open savings account");
+
+        }
+        else
+        {
+        printf("You are not eligible to open savings account");
+        }
+    }
+    else
+    {
+        printf("Invalid age");
+    }
+}
