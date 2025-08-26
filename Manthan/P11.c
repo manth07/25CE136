b96f17951dda602a89ec8fb413ec0205b93b5df4
Manthan <manthanvasoya07@gmail.com>
2025-08-26 10:37:32 +0530

    Delete Manthan/P11.c

--- Manthan/P11.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int a;
-    printf("Enter your marks(0-100): \n");
-    scanf("%d",&a);
-    (a>89)?printf("Your grade is A"):(a<90&&a>79)?printf("Your grade is B"):(a<80&&a>69)?printf("Your grade is C"):(a<70&&a>59)?printf("Your grade is D"):printf("You failed");
-}
