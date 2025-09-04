7df671550eb5c11055f6cc0dc32f188d9aa02d85
Manthan <manthanvasoya07@gmail.com>
2025-09-04 15:57:13 +0530

    Add files via upload

---
+++ Manthan/P20.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    int i,n,present=0,expected,missing;
+
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+
+    int arr[n-1];
+
+    for(i=0;i<n-1;i++)
+    {
+        printf("Enter arr[%d] = ",i);
+        scanf("%d",&arr[i]);
+        present=present+arr[i];
+    }
+
+    expected=(n*(n+1))/2;
+    missing=expected-present;
+    printf("The missing ID is: %d",missing);
+}
