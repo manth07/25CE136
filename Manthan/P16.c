efdfcc92586fc9999384b53988adb474090df737
Manthan <manthanvasoya07@gmail.com>
2025-09-04 15:32:59 +0530

    Add files via upload

---
+++ Manthan/P16.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+int main()
+{
+    int us,cs,sticks;
+    sticks=21;
+
+    while(1)
+    {
+        printf("Select the number of sticks:");
+        scanf("%d",&us);
+        if(us>0&&us<5)
+        {
+            if(us==4)
+            {
+                printf("User selected 4 sticks\n");
+                printf("Computer selected 1 stick\n");
+                sticks=sticks-5;
+            }
+            else if(us==3)
+            {
+                printf("User selected 3 sticks\n");
+                printf("Computer selected 2 sticks\n");
+                sticks=sticks-5;
+            }
+            else if(us==2)
+            {
+                printf("User selected 2 sticks\n");
+                printf("Computer selected 3 sticks\n");
+                sticks=sticks-5;
+            }
+            else if(us==1)
+            {
+                printf("User selected 1 sticks\n");
+                printf("Computer selected 4 sticks\n");
+                sticks=sticks-5;
+            }
+            if(sticks==1)
+            {
+                printf("User selected 1 stick");
+                printf("\nComputer won!");
+                break;
+            }
+        }
+        else
+        {
+            printf("Please choose number of sticks between 1 to 4");
+        }
+    }
+}
