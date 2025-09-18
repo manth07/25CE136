371e292fa3af47964c316a9df924634d242fa94a
Manthan <manthanvasoya07@gmail.com>
2025-09-18 16:19:19 +0530

    Add files via upload

---
+++ Manthan/P26.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+int is_valid_triangle(float a, float b, float c)
+{
+    return (a + b > c && a + c > b && b + c > a);
+}
+
+float calculate_area(float a, float b, float c)
+{
+    float s = (a + b + c) / 2;
+    float area = sqrtf(s * (s - a) * (s - b) * (s - c));
+    return area;
+}
+
+int main()
+{
+    float a, b, c;
+
+    // Input handled only in main
+    printf("Enter the length of side a: ");
+    scanf("%f", &a);
+    printf("Enter the length of side b: ");
+    scanf("%f", &b);
+    printf("Enter the length of side c: ");
+    scanf("%f", &c);
+
+    if (is_valid_triangle(a, b, c))
+    {
+        float area = calculate_area(a, b, c);
+        printf("The sides form a valid triangle.\n");
+        printf("The area of the triangle is: %.2f square units.\n", area);
+    }
+    else
+    {
+        printf("The sides do not form a valid triangle.\n");
+    }
+
+    return 0;
+}
