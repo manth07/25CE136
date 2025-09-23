ae824626ff024dbf97cf89017a4402be5ac64469
Manthan <manthanvasoya07@gmail.com>
2025-09-23 10:50:08 +0530

    Add files via upload

---
+++ Manthan/P19(3).c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main()
+
+{
+    int i,j,s,n = 5;
+    for(i=n;i>=1;i--)
+    {
+
+        for(s=0;s<n-i;s++)
+            printf("  ");
+        for(j=i;j>=1;j--)
+            printf("%d ",j);
+        for(j=2;j<=i;j++)
+            printf("%d ",j);
+        printf("\n");
+
+    }
+
+    for(i=2;i<=n;i++)
+    {
+        for(s=0;s<n-i;s++)
+            printf("  ");
+        for(j=i;j>=1;j--)
+            printf("%d ",j);
+        for(j=2;j<=i;j++)
+            printf("%d ",j);
+        printf("\n");
+    }
+
+}
