dc782cc3e3f2a034541c583c289b3eab6066f97f
Manthan <manthanvasoya07@gmail.com>
2025-09-23 10:06:50 +0530

    Add files via upload

---
+++ Manthan/P19(1).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,n;
+    printf("Enter n: ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            if(j%2==0)
+                printf("0 ");
+            else
+                printf("1 ");
+        }
+        printf("\n");
+    }
+
+}
