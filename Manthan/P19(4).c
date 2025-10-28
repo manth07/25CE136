33732db510abfcd1615bd43393f547afe62f8ea2
Manthan <manthanvasoya07@gmail.com>
2025-10-28 09:10:58 +0530

    Add files via upload

---
+++ Manthan/P19(4).c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,s;
+
+    printf("enter number you want to make pattern(0to9):\n");
+    scanf("%d",&n);
+    for(i=1; i<=n; i++)
+    {
+        for(s=1; s<=n-i; s++)
+            printf(" ");
+        for(j=1; j<=i; j++)
+            printf("%c",'A'+j-1);
+        for(j=i-1; j>=1; j--)
+            printf("%c",'A'+j-1);
+             printf("\n");
+    }
+    for(i=n-1; i>=1; i--)
+ {
+        for(s=1; s<=n-i; s++)
+            printf(" ");
+        for(j=1; j<=i; j++)
+            printf("%c",'A'+j-1);
+        for(j=i-1; j>=1; j--)
+            printf("%c",'A'+j-1);
+              printf("\n");
+    }
+
+
+return 0;
+}
