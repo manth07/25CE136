ea238827c52f9a021dcc2bf4debcc207210ebe0f
Manthan <manthanvasoya07@gmail.com>
2025-11-13 23:54:35 +0530

    Refactor note handling functionality

--- Manthan/P24.c
+++ Manthan/P24.c
@@ -1,153 +1,123 @@
-#include <stdio.h>
-
-int main() {
-    int choice;
-    char s[1000], r[1000], b[1000], copy[1000];
-    char str1[1000], str2[1000], str[2000];
-    char low[1000], upper[1000], cap[1000];
-    int i, length = 0, len = 0, len1 = 0, len2 = 0, equal;
-
-    do {
-        printf("\nChoice 1:Length of the note\n");
-        printf("Choice 2:Reverse note\n");
-        printf("Choice 3:Compare two notes\n");
-        printf("Choice 4:Copy a note\n");
-        printf("Choice 5:Concatenate a note\n");
-        printf("Choice 6:Upper case converter\n");
-        printf("Choice 7:Lower case converter\n");
-        printf("Choice 8:Capitalize word\n");
-        printf("Choice 9:Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                printf("Enter a string: ");
-                scanf("%s", s);
-                length = 0;
-                for (i = 0; s[i] != '\0'; i++) {
-                    length = i + 1;
-                }
-                printf("Length of the string: %d\n", length);
-                break;
-
-            case 2:
-                if (length == 0) {
-                    printf("Please enter a string first (Choice 1).\n");
-                    break;
-                }
-                for (i = 0; i < length; i++) {
-                    r[i] = s[length - 1 - i];
-                }
-                r[length] = '\0';
-                printf("\nThe reverse of the note is: %s\n", r);
-                break;
-
-            case 3:
-                if (length == 0) {
-                    printf("Please enter a string first (Choice 1).\n");
-                    break;
-                }
-                printf("\nEnter another note: ");
-                scanf("%s", b);
-                len = 0;
-                for (i = 0; b[i] != '\0'; i++) {
-                    len = i + 1;
-                }
-                equal = 1;
-                if (len == length) {
-                    for (i = 0; i < length; i++) {
-                        if (s[i] != b[i]) {
-                            equal = 0;
-                            break;
-                        }
-                    }
-                } else {
-                    equal = 0;
-                }
-                if (equal == 1) {
-                    printf("Equal\n");
-                } else {
-                    printf("Not equal\n");
-                }
-                break;
-
-            case 4:
-                if (length == 0) {
-                    printf("Please enter a string first (Choice 1).\n");
-                    break;
-                }
-                for (i = 0; i < length; i++) {
-                    copy[i] = s[i];
-                }
-                copy[length] = '\0';
-                printf("\nThe copied note is %s\n", copy);
-                break;
-
-            case 5:
-                printf("\nEnter first note: ");
-                scanf("%s", str1);
-                printf("\nEnter second note: ");
-                scanf("%s", str2);
-                len1 = 0;
-                for (i = 0; str1[i] != '\0'; i++) {
-                    len1 = i + 1;
-                }
-                len2 = 0;
-                for (i = 0; str2[i] != '\0'; i++) {
-                    len2 = i + 1;
-                }
-                for (i = 0; i < len1; i++) {
-                    str[i] = str1[i];
-                }
-                for (i = 0; i < len2; i++) {
-                    str[len1 + i] = str2[i];
-                }
-                str[len1 + len2] = '\0';
-                printf("The combined notes are: %s\n", str);
-                break;
-
-            case 6:
-                printf("\nEnter lowercase note: ");
-                scanf("%s", low);
-                for (i = 0; low[i] != '\0'; i++) {
-                    if (low[i] >= 'a' && low[i] <= 'z')
-                        low[i] = low[i] - 32;
-                }
-                printf("the uppercase of note is : %s\n", low);
-                break;
-
-            case 7:
-                printf("\nEnter uppercase note: ");
-                scanf("%s", upper);
-                for (i = 0; upper[i] != '\0'; i++) {
-                    if (upper[i] >= 'A' && upper[i] <= 'Z')
-                        upper[i] = upper[i] + 32;
-                }
-                printf("the lowercase of note is : %s\n", upper);
-                break;
-
-            case 8:
-                printf("\nEnter lowercase note: ");
-                scanf("%s", cap);
-                if (cap[0] >= 'a' && cap[0] <= 'z') {
-                    cap[0] = cap[0] - 32;
-                }
-                printf("The capitalized note is: %s\n", cap);
-                break;
-
-            case 9:
-                printf("Exiting...\n");
-                break;
-
-            default:
-                printf("Invalid choice.\n");
-                break;
+#include<stdio.h>
+#include<unistd.h>
+#include<ctype.h>
+#include<math.h>
+int main()
+{
+    int i,len,len2,ta,fl;
+    char str[200],str2[200],ch='y';
+    printf("Enter your jot down note here:");
+    gets(str);
+    while(ch=='y')
+    {
+        printf("\n\n\nENTER THE TASK TO PERFORM FROM BELOW:");
+        printf("\n1.Length");
+        printf("\n2.Reverse");
+        printf("\n3.Compare two strings");
+        printf("\n4.Copy to other");
+        printf("\n5.Concatenate two");
+        printf("\n6.Convert to upper case");
+        printf("\n7.Convert to lower case");
+        printf("\n8.Capitalize each words\n9.Exit\n");
+        printf("\nenter the task id: ");
+        scanf("%d",&ta);
+        if(ta==1)
+        {
+            printf("Calculating length of your note!!\n");
+            sleep(2);
+            for(i=0;str[i]!='\0';i++);
+            printf("%d is your length!!\n",i);
         }
-    } while (choice != 9);
-
+        else if(ta==2)
+        {
+            printf("Doing reverse:\n");
+            sleep(2);
+            for(i=0;str[i]!='\0';i++);
+            len=i;
+            for(i=len-1;i>=0;i--)
+                printf(" %c",str[i]);
+            printf("\n");
+        }
+        else if(ta==3)
+        {
+            printf("Comparing 2 strings");
+            printf("\nEnter the other string!");
+            scanf(" %s",str2);
+            fl=1;
+            for(i=0;i<=200;i++)
+            {
+                if(str[i]!=str2[i])
+                    fl=0;
+            }
+            if(fl=0)
+                printf("NOT EQUAL");
+            else
+                printf("EQUAL");
+        }
+        else if(ta==4)
+        {
+            printf("Copying...\n");
+            sleep(2);
+            for(i=0;str[i]!='\0';i++)
+                str2[i]=str[i];
+            str2[i]='\0';
+            printf("Copied string: %s",str2);
+        }
+        else if(ta==5)
+        {
+            printf("Enter the other string to concatenate");
+            scanf("%s",str2);
+            for(i=0;str[i]!='\0';i++);
+            len=i;
+            printf("concatenating....\n");
+            sleep(2);
+            for(i=0;str2[i]!='\0';i++)
+                str[len+i]=str2[i];
+            str[len+i]='\0';
+            printf("New string:  %s",str);
+        }
+        else if(ta==6)
+        {
+            printf("Your string: %s",str);
+            printf("\nConverting to upper case...\n");
+            sleep(2);
+            for(i=0;str[i]!='\0';i++)
+                str[i]=toupper(str[i]);
+            printf("Successfully converted to %s",str);
+        }
+        else if(ta==7)
+        {
+            printf("Your string: %s",str);
+            printf("\nConverting to lower case...\n");
+            sleep(2);
+            for(i=0;str[i]!='\0';i++)
+                str[i]=tolower(str[i]);
+            printf("Successfully converted to %s",str);
+        }
+        else if(ta==8)
+        {
+            printf("Capitalizing each words...\n");
+            sleep(2);
+            for(i=0;str[i]!='\0';i++)
+            {
+                str[0]=toupper(str[0]);
+                if(str[i]==' ')
+                    str[i+1]=toupper(str[i+1]);
+            }
+            printf("Succesfully converted to %s",str);
+        }
+        else if(ta==9)
+        {
+            printf("Thank you!!\n");
+            break;
+        }
+        else
+        {
+            printf("Enter valid choice!!\n");
+        }
+    }
     printf("\nName: Manthan Vasoya\n");
-    printf("ID: 25CE136\n");
+    printf("ID: 25CE136");
 
     return 0;
 }
