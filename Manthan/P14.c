0f4405d19c856f7a7c67e2e9864d402e3c61e3f8
Manthan <manthanvasoya07@gmail.com>
2025-11-13 20:51:42 +0530

    Refactor ATM program to use do-while loop

--- Manthan/P14.c
+++ Manthan/P14.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int wa,tb,c;
+    int wa,tb,choice;
     tb=5000;
     printf("Your Total Balance is: %d\n",tb);
-    while(1)
+    do
     {
 
         printf("Choice 1: Withdraw \nChoice 2: Exit\n");
         printf("Enter Choice: \n");
-        scanf("%d",&c);
-
-        if(c==1)
+        scanf("%d",&choice);
+        switch(choice)
         {
+
+        case 1:
             printf("Enter the amount to be withdrawn:");
             scanf("%d",&wa);
             if (wa<=tb && wa>0)
@@ -22,33 +23,28 @@ int main()
 
                 printf("Choice 1: Withdraw \nChoice 2: Exit\n");
                 printf("Enter Choice: \n");
-                scanf("%d",&c);
+                scanf("%d",&choice);
 
                 if(tb==0)
                 {
                     printf("You cannot withdraw anymore\n");
-                    break;
                 }
-        }
+            }
             else
             {
                 printf("Balance is not sufficient\n");
             }
-        }
-        else if(c==2)
-        {
+            break;
+
+        case 2:
+
             printf("Thanks for the visit");
             break;
         }
-        else
-        {
-            printf("Invalid Choice\n");
-        }
+    }while(choice!=2);
 
-    }
     printf("\nName: Manthan Vasoya\n");
     printf("ID: 25CE136");
     return 0;
 
 }
-
