4bb990b4b04b79b61166e79aaac227fe82029c09
Manthan <manthanvasoya07@gmail.com>
2025-11-13 22:59:53 +0530

    Refactor P21.c to count number categories
    Updated the program to read 25 integers and count positive, negative, even, and odd numbers.

--- Manthan/P21.c
+++ Manthan/P21.c
@@ -1,13 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
+int main() {
+    int num[25];
+    int positiveC = 0;
+    int negativeC = 0;
+    int evenC = 0;
+    int oddC = 0;
+    printf("Enter 25 integers for the filter:\n");
+    for (int i = 0; i < 25; i++) {
+        printf("Number %d: ", i + 1);
+        scanf("%d", &num[i]);
+        if (num[i] > 0) {
+            positiveC++;
+        } else if (num[i] < 0) {
+            negativeC++;
+        }
 
-void main()
-{
-    int n[50],i;
-    printf("Please enter the 25 numbers");
-    for(i=0;i<10;i++)
-    {
-        scanf("%d",&n);
+        if (num[i] % 2 == 0) {
+            evenC++;
+        } else {
+            oddC++;
+        }
     }
+    printf("Total positive numbers are: %d\n", positiveC);
+    printf("Total negative numbers are: %d\n", negativeC);
+    printf("Total even numbers are: %d\n", evenC);
+    printf("Total odd numbers are: %d\n", oddC);
 
-
+    return 0;
 }
