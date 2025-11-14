602b0c53b24742899cdb7617c4b42ab960321e8e
Manthan <manthanvasoya07@gmail.com>
2025-11-14 13:01:57 +0530

    Add files via upload

---
+++ Manthan/P30.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int n,i,j;
+    float *p,*q,*t;
+
+    printf("Enter the no. of items: ");
+    scanf("%d",&n);
+    if(n<=0)
+    {
+        printf("No items\n");
+        return 0;
+    }
+    p=(float *)calloc(n,sizeof(float));
+    t=p;
+
+    for(i=1;i<=n;i++,p++)
+    {
+        printf("Enter the price of item %d: ",i);
+        scanf("%f",p);
+    }
+    p=t;
+
+    for(i=1;i<n;i++,p++)
+    {
+        for(j=i+1,q=p+1;j<=n;j++,q++)
+        {
+            if(*p>*q)
+            {
+                *p=*p+*q;
+                *q=*p-*q;
+                *p=*p-*q;
+            }
+        }
+    }
+
+    printf("Prices in ascending order are:\n");
+    for(i=1;i<=n;i++,t++)
+    {
+        printf("\n%.2f ",*t);
+    }
+    free(t);
+    free(p);
+    free(q);
+
+
+}
