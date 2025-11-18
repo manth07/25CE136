4507afec03c2d635aab34a2014488dc2a142872f
Manthan <manthanvasoya07@gmail.com>
2025-11-18 09:58:26 +0530

    Add files via upload

---
+++ Manthan/P32.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void main()
+{
+    FILE *f,*t;
+    char ch;
+
+    f=fopen("data.txt","r");
+    if(f==NULL)
+        printf("cant't open file");
+    else
+    {
+        fseek(f,-1,SEEK_END);
+        while(1)
+        {
+            ch=fgetc(f);
+            printf("%c",ch);
+            if(fseek(f,-2,SEEK_CUR))
+            {
+                break;
+            }
+        }
+    }
+    fclose(f);
+
+}
+
