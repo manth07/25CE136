7bb425935b38b58a96cdc96cc8785a2159a57bc2
Manthan <manthanvasoya07@gmail.com>
2026-01-22 14:33:20 +0530

    Add files via upload

---
+++ C++/LAB/Hello.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int rollnum;
+    string name;
+    cout<<"\t\tCHAROTAR UNIVERSITY OF SCIENCE & TECHNOLOGY\n";
+    cout<<"\t\tSEMESTER GRADE REPORT\n";
+    cout<<"\t\tFACULTY OF TECHNOLOGY AND ENGINEERING\n";
+    cout<<"Name	        :	MANTHAN H VASOYA\t\t ID. No.	:	25CE136\n";
+    cout<<"Programme	:	B.TECH. (COMPUTER)\n";
+    cout<<"Month & Year	:	December 2025\t\t\t Semester  : 1\n\n";
+    cout<<"Course	                                                                Credit (C)	    CCE (Theory)	SEE (Theory)	CCE (Practical)	SEE (Practical)	AGG. GRADE\n";
+    cout<<"CEUC101	COMPUTER CONCEPT AND PROGRAMMING                                    5.00	        B	            A	                A+	            A	        B+\n";
+    cout<<"EEUD101	BASICS OF ELECTRONICS AND ELECTRICAL ENGINEERING                    4.00	        A	            A+	                A+	            A	        A+\n";
+    cout<<"FTUC101	FOUNDATION COURSE ON MATHEMATICS AND PHYSICS (AUDIT COURSE)          --	            --          	--              	--          	A+      	A+\n";
+    cout<<"HSUA101	COMMUNICATIVE ENGLISH                                               2.00        	--          	--              	A           	A       	A\n";
+    cout<<"ITUS101	ICT WORKSHOP                                                        2.00        	--          	--              	A           	O       	A+\n";
+    cout<<"MSUD101	ENGINEERING MATHEMATICS-I                                           4.00           	A           	A+              	--          	--      	A\n";
+    cout<<"PSUD101	ENGINEERING PHYSICS-I                                               2.00        	--          	--              	A+          	A       	A+\n";
+
+    return 0;
+
+
+}
